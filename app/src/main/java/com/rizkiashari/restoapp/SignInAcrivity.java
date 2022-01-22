47825df10a26ab3ca70f0cc3add4e433a7e3c2ee
RizkiAshari <rizkiashari1026@gmail.com>
2022-01-22 20:56:11 +0700

    initial project and done add sign in set account kit

---
+++ app/src/main/java/com/rizkiashari/restoapp/SignInAcrivity.java
@@ -0,0 +1,103 @@
+package com.rizkiashari.restoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+import com.huawei.hms.support.api.entity.common.CommonConstant;
+import com.huawei.hms.support.hwid.ui.HuaweiIdAuthButton;
+
+public class SignInAcrivity extends AppCompatActivity {
+
+    HuaweiIdAuthButton huaweiIdAuthButton;
+
+    private AccountAuthService mAuthService;
+
+    private AccountAuthParams mAuthParam;
+
+    private static final int REQUEST_CODE_SIGN_IN = 1000;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        huaweiIdAuthButton = findViewById(R.id.huaweiIdAuthButton);
+        huaweiIdAuthButton.setOnClickListener(v -> {
+            signInHwId();
+        });
+    }
+
+    private void signInHwId() {
+        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setEmail()
+                .createParams();
+
+        mAuthService = AccountAuthManager.getService(this, mAuthParam);
+
+        Task<AuthAccount> task = mAuthService.silentSignIn();
+        task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
+            @Override
+            public void onSuccess(AuthAccount authAccount) {
+               dealWithResultOfSignIn(authAccount);
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                if (e instanceof ApiException) {
+                    ApiException apiException = (ApiException) e;
+                    Intent signInIntent = mAuthService.getSignInIntent();
+                    startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+                }
+            }
+        });
+    }
+
+    private void dealWithResultOfSignIn(AuthAccount authAccount) {
+        // Obtain the HUAWEI DI information.
+        Log.i(TAG, "display name:" + authAccount.getDisplayName());
+        Log.i(TAG, "photo uri string:" + authAccount.getAvatarUriString());
+        Log.i(TAG, "photo uri:" + authAccount.getAvatarUri());
+        Log.i(TAG, "email:" + authAccount.getEmail());
+        Log.i(TAG, "openid:" + authAccount.getOpenId());
+        Log.i(TAG, "unionid:" + authAccount.getUnionId());
+
+        Intent i = new Intent(SignInAcrivity.this, MainActivity.class);
+        i.putExtra("name", authAccount.getDisplayName());
+        i.putExtra("pic", authAccount.getAvatarUriString());
+        i.putExtra("email", authAccount.getEmail());
+        startActivity(i);
+        finish();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
+            Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+            if (authAccountTask.isSuccessful()) {
+                AuthAccount authAccount = authAccountTask.getResult();
+                dealWithResultOfSignIn(authAccount);
+                Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
+            } else {
+                Log.e(TAG, "sign in failed : " +((ApiException)authAccountTask.getException()).getStatusCode());
+            }
+        }
+    }
+}
