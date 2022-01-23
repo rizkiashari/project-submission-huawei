6351542dc322e289d890f07c9eaf8a78ca9b06d3
RizkiAshari <rizkiashari1026@gmail.com>
2022-01-23 20:29:02 +0700

    done splash screen

--- app/src/main/java/com/rizkiashari/restoapp/SplashScreen.java
+++ app/src/main/java/com/rizkiashari/restoapp/SplashScreen.java
@@ -2,13 +2,23 @@ package com.rizkiashari.restoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(()->{
+            Intent signIntent = new Intent(SplashScreen.this, SignInAcrivity.class);
+            startActivity(signIntent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH );
     }
 }
