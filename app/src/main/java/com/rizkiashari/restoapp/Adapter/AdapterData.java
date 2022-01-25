840875a53069e161f0ff4d6ae78733aa597c93dd
RizkiAshari <rizkiashari1026@gmail.com>
2022-01-25 20:20:46 +0700

    ...

--- app/src/main/java/com/rizkiashari/restoapp/Adapter/AdapterData.java
+++ app/src/main/java/com/rizkiashari/restoapp/Adapter/AdapterData.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.rizkiashari.restoapp.R;
 import com.rizkiashari.restoapp.model.DataModel;
 
+import java.net.URL;
 import java.util.List;
 
 public class AdapterData extends RecyclerView.Adapter<AdapterData.HolderData> {
